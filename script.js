9efe2d252e92c3f51ba184c5520cd61eaf0503f6
BraydonP <braydon01@hotmail.com>
2020-02-17 12:57:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-// coded by @lasjorg
-// eslint-disable-next-line no-unused-vars
+
 const projectName = "portfolio";
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+
+mainNavLinks.forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        let target = document.querySelector(event.target.hash);
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
