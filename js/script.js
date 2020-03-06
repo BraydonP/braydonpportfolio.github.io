e165a868c63468118eff2ea0dd391c4f6a329463
BraydonP <braydon01@hotmail.com>
2020-03-06 13:19:14 +1100

    commit master

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
 
 const projectName = "portfolio";
-document.querySelector('.hello').scrollIntoView({ 
-  behavior: 'smooth' 
-});
-let mainNavLinks = document.querySelectorAll("nav ul li a");
 
-mainNavLinks.forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-        let target = document.querySelector(event.target.hash);
-        target.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    });
-});
